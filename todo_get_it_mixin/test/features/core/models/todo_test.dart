c28f6035a11c1c28d63539e08a0d1d1e653abe05
Paurakh Sharma Humagain <paurakh011@gmail.com>
2023-04-11 20:46:38 +0545

    Add tests for Todo model and StoreService

---
+++ todo_get_it_mixin/test/features/core/models/todo_test.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:todo_get_it_mixin/features/core/models/todo.dart';
+
+import '../../../fixtures/fixture_reader.dart';
+
+void main() {
+  group('fromJson', () {
+    test(
+      'should be able to create a model from a json map',
+      () async {
+        // arrange
+        final jsonString = fixture('todo.json');
+        final map = json.decode(jsonString);
+        // act
+        final result = Todo.fromJson(map);
+        // assert
+        expect(result, isA<Todo>());
+        expect(result.title, map['title']);
+        expect(result.description, map['description']);
+        expect(result.isCompleted, map['isCompleted']);
+      },
+    );
+  });
+
+  group('toJson', () {
+    test(
+      'should be able to export the model to json',
+      () async {
+        // arrange
+        final jsonString = fixture('todo.json');
+        final map = json.decode(jsonString);
+        final episode = Todo.fromJson(map);
+        // act
+        final result = episode.toJson();
+        // assert
+        expect(result, isA<Map<String, dynamic>>());
+        expect(result['title'], map['title']);
+        expect(result['description'], map['description']);
+        expect(result['isCompleted'], map['isCompleted']);
+      },
+    );
+  });
+}
---
+++ todo_get_it_mixin/test/features/core/services/store_service_test.dart
@@ -0,0 +1,102 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hive/hive.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:todo_get_it_mixin/features/core/services/store_service.dart';
+
+import 'store_service_test.mocks.dart';
+
+@GenerateMocks([Box])
+void main() {
+  late StoreService storeService;
+  late MockBox mockBox;
+
+  setUp(() {
+    mockBox = MockBox();
+    storeService = StoreService(mockBox);
+  });
+
+  group('save', () {
+    const tKey = 'key';
+    const tValue = {"title": "Title"};
+
+    test(
+      'should jsonEncode the value while saving it in the store',
+      () async {
+        // arrange
+        when(mockBox.put(any, any)).thenAnswer((_) async => {});
+
+        // act
+        await storeService.save(tKey, tValue);
+
+        // assert
+        final encodedValue = jsonEncode(tValue);
+        verify(mockBox.put(tKey, encodedValue));
+      },
+    );
+
+    test(
+      'should throw an exception if the store throws an exception',
+      () async {
+        // arrange
+        when(mockBox.put(any, any)).thenThrow(Exception());
+
+        // act
+        final call = storeService.save;
+
+        // assert
+        expect(() => call(tKey, tValue), throwsException);
+      },
+    );
+  });
+
+  group('get', () {
+    const tKey = 'key';
+    const tValue = '{"title": "Title"}';
+
+    test(
+      'should jsonDecode the value while getting it from the store',
+      () async {
+        // arrange
+        when(mockBox.get(any)).thenReturn(tValue);
+
+        // act
+        final result = storeService.get(tKey);
+
+        // assert
+        final decodedValue = jsonDecode(tValue);
+        expect(result, decodedValue);
+      },
+    );
+
+    test(
+      'should return null if the store returns null',
+      () async {
+        // arrange
+        when(mockBox.get(any)).thenReturn(null);
+
+        // act
+        final result = storeService.get(tKey);
+
+        // assert
+        expect(result, null);
+      },
+    );
+
+    test(
+      'should throw an exception if the store throws an exception',
+      () async {
+        // arrange
+        when(mockBox.get(any)).thenThrow(Exception());
+
+        // act
+        final call = storeService.get;
+
+        // assert
+        expect(() => call(tKey), throwsException);
+      },
+    );
+  });
+}
