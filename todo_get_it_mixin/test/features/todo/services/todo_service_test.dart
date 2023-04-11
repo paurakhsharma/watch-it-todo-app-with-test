3003184a32be71bd0bc9c475f75e169e51e74c69
Paurakh Sharma Humagain <paurakh011@gmail.com>
2023-04-11 20:58:20 +0545

    add test for TodoService

--- todo_get_it_mixin/test/features/todo/services/todo_service_test.dart
+++ todo_get_it_mixin/test/features/todo/services/todo_service_test.dart
@@ -1,8 +1,14 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:todo_get_it_mixin/features/core/models/todo.dart';
 import 'package:todo_get_it_mixin/features/core/services/store_service.dart';
 import 'package:todo_get_it_mixin/features/todo/services/todo_service.dart';
 
+import '../../../fixtures/fixture_reader.dart';
 import 'todo_service_test.mocks.dart';
 
 @GenerateMocks([StoreService])
@@ -15,5 +21,56 @@ void main() {
     todoService = TodoService(mockStoreService);
   });
 
-  group('save', () {});
+  group('saveTodos', () {
+    final tTodos = [const Todo(id: 'id', title: 'Todo Title')];
+
+    test('should call StoreService.save with correct data', () async {
+      // act
+      await todoService.saveTodos(tTodos);
+
+      // assert
+      verify(mockStoreService.save(todoService.todosKey, tTodos));
+    });
+
+    test('should throw an exception if StoreService.save throws an exception',
+        () async {
+      // arrange
+      when(mockStoreService.save(any, any)).thenThrow(Exception());
+
+      // act
+      final call = todoService.saveTodos;
+
+      // assert
+      expect(() => call(tTodos), throwsException);
+    });
+  });
+
+  group('getTodos', () {
+    final tTodos = jsonDecode(fixture('todos.json'));
+
+    test('should call StoreService.get with correct data', () async {
+      // arrange
+      when(mockStoreService.get(any)).thenReturn(tTodos);
+
+      // act
+      final result = todoService.getTodos();
+
+      // assert
+      verify(mockStoreService.get(todoService.todosKey));
+      final todos = (tTodos as List).map((e) => Todo.fromJson(e)).toList();
+      assert(listEquals(result, todos) == true);
+    });
+
+    test('should return an empty list if StoreService.get returns null',
+        () async {
+      // arrange
+      when(mockStoreService.get(any)).thenReturn(null);
+
+      // act
+      final result = todoService.getTodos();
+
+      // assert
+      expect(result, []);
+    });
+  });
 }
